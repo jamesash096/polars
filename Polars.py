9c082b08bcbdc7785fe9301c9e55f5b68ed5d5a2
James Ashwin <54464495+jamesash096@users.noreply.github.com>
2025-05-10 14:30:47 -0500

    Added files for Polars research project

---
+++ Polars.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Polars v Pandas", 
+    layout="wide", 
+    page_icon="./icons/polars_logo.png"
+)
+
+st.title("Polars: DataFrames for the new era")
+
+st.markdown("With today's data driven world increasing the scale of datasets all over, Pandas is unable to handle processing such a massive load of data")
+st.markdown("Enter Polars. Faster, Leaner and Better")
